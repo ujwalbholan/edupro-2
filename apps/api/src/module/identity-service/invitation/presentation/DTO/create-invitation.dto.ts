3b10b406b851d3dcdcf7513181a6e104a055550d
ujwal bholan <160625871+ujwalbholan@users.noreply.github.com>
2026-09-26 22:36:09 +0545

    feat: configured mailpit

--- apps/api/src/module/identity-service/invitation/presentation/DTO/create-invitation.dto.ts
+++ apps/api/src/module/identity-service/invitation/presentation/DTO/create-invitation.dto.ts
@@ -1,10 +1,8 @@
 import { z } from 'zod';
 
 export const createInvitationSchema = z.object({
-  tenantId: z.uuid(),
   email: z.email(),
   roleId: z.uuid(),
-  invitedByUserId: z.uuid(),
 });
 
 export type CreateInvitationDto = z.infer<typeof createInvitationSchema>;
